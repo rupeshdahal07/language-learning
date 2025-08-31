628b8d77acac0ebece7002a7751ae027ce15078f
rupeshdahal07 <rupyesdahal12@gmail.com>
2025-08-31 09:27:03 +0545

    env changes

--- app/supabase_client.py
+++ app/supabase_client.py
@@ -1,14 +1,21 @@
 # myapp/supabase_client.py
 import os
+from config.env import env, BASE_DIR
 from supabase import create_client, Client
 
-url: str = "http://64.227.141.251:8000"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzU0ODQ5NzAwLCJleHAiOjE5MTI2MTYxMDB9.Sb8f7UBYH1JncIawaFImCPgxG5d6Ljk3lPOb904kdC8"
+
+
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# url: str = "http://64.227.141.251:8000"
+url: str = env('SUPABASE_URL')
+# key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzU0ODQ5NzAwLCJleHAiOjE5MTI2MTYxMDB9.Sb8f7UBYH1JncIawaFImCPgxG5d6Ljk3lPOb904kdC8"
+key: str = env('SUPABASE_KEY')
 supabase: Client = create_client(url, key)
 
 auth = supabase.auth.sign_in_with_password({
-    "email": "rupyesdahal12@gmail.com",
-    "password": "rupesh123"
+    "email": env('SUPABASE_EMAIL'),
+    "password": env('SUPABASE_PASSWORD')
 })
 access_token = auth.session.access_token
 supabase.postgrest.auth(access_token)
---
+++ config/env.py
@@ -0,0 +1,9 @@
+import environ
+
+from pathlib import Path
+
+
+env = environ.Env()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
