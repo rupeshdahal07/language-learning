9c49e5e5ed4214e95d4fa92a37b2be5d7db9bd84
rupeshdahal07 <rupyesdahal12@gmail.com>
2025-09-15 11:55:25 +0545

    supabase

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import UserPathProgressView, UserLessonProgressView, UserLevelProgressView
+from .views import UserPathProgressView, UserLessonProgressView, UserLevelProgressView, UserRegistration
 
 urlpatterns = [
     # User Path Progress CRUD operations
     path('user-path-progress/', UserPathProgressView.as_view(), name='user-path-progress'),
     path('user-lesson-progress/', UserLessonProgressView.as_view(), name='user-lesson-progress'),
     path('user-level-progress/', UserLevelProgressView.as_view(), name='user-level-progress'),
+    path('user-create/', UserRegistration.as_view(), name='user-create'),
     
     # Bulk operations for adding completed lessons
     #path('user-path-progress/add-lesson/', UserPathProgressBulkView.as_view(), name='user-path-progress-add-lesson'),
